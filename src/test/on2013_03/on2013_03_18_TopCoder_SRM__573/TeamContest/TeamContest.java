14de23e294894eb12ac99bb339a8bf8469f786e6
Yuanming Yu <yyming@gmail.com>
2013-03-18 20:23:08 +0800

    Functionality test
    CHelper: Launch, Submit, Archive, Run Test;
    GitHub: Commit and push

---
+++ src/test/on2013_03/on2013_03_18_TopCoder_SRM__573/TeamContest/TeamContest.java
@@ -0,0 +1,39 @@
+package test.on2013_03.on2013_03_18_TopCoder_SRM__573.TeamContest;
+
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TeamContest {
+    public int worstRank(int[] strength) {
+        int myScore = Math.max(strength[0], Math.max(strength[1], strength[2]))
+                + Math.min(strength[0], Math.min(strength[1], strength[2]));
+
+        List<Integer> Q = new ArrayList<Integer>();
+        for (int i=3; i<strength.length; ++i)
+            Q.add(Integer.valueOf(strength[i]));
+        Collections.sort(Q);
+
+        int ret = 0;
+        while (!Q.isEmpty()) {
+            int first = Q.get(Q.size()-1); Q.remove(Q.size()-1);
+
+            boolean found = false;
+            for (int i=0; i<Q.size()-1; ++i)
+                if (first + Q.get(i) > myScore) {
+                    ++ret;
+                    Q.remove(i);
+                    Q.remove(i);
+                    found = true;
+                    break;
+                }
+            if (!found) break;
+        }
+
+        return 1 + ret;
+    }
+}
+
+
